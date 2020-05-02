e997b293d962d458b226833d1c8cb9e32b759459
pkmango <pk-man@yandex.ru>
2020-05-02 23:58:59 +0300

    Movement. First commit

---
+++ Movement/Movement/Assets/MovingSphere.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MovingSphere : MonoBehaviour
+{
+    void Update()
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        transform.localPosition = new Vector3(playerInput.x, 0f, playerInput.y);
+    }
+}
