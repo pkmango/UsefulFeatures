868e502441f364f69f730a09a74260a4ff9eac8a
pkmango <pk-man@yandex.ru>
2020-05-03 09:26:07 +0300

    Velocity. Speed. Acceleration.

--- Movement/Movement/Assets/MovingSphere.cs
+++ Movement/Movement/Assets/MovingSphere.cs
@@ -2,12 +2,24 @@
 
 public class MovingSphere : MonoBehaviour
 {
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f;
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 10f;
+
+    Vector3 velocity;
+
     void Update()
     {
         Vector2 playerInput;
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        transform.localPosition = new Vector3(playerInput.x, 0f, playerInput.y);
+        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
+        velocity.z =  Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+        Vector3 displacement = velocity * Time.deltaTime;
+        transform.localPosition += displacement;
     }
 }
