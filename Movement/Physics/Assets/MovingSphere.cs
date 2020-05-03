5abf36e4190d0386ab965692e65313466ac8b706
pkmango <pk-man@yandex.ru>
2020-05-03 23:06:02 +0300

    Added jumping

--- Movement/Physics/Assets/MovingSphere.cs
+++ Movement/Physics/Assets/MovingSphere.cs
@@ -6,9 +6,16 @@ public class MovingSphere : MonoBehaviour
     float maxSpeed = 10f;
     [SerializeField, Range(0f, 100f)]
     float maxAcceleration = 10f;
+    [SerializeField, Range(0f, 10f)]
+    float jumpHeight = 2f;
+    [SerializeField, Range(0, 5)]
+    int maxAirJumps = 0;
 
-    Vector3 velocity;
+    Vector3 velocity, desiredVelocity;
     Rigidbody body;
+    bool desiredJump;
+    bool onGround;
+    int jumpPhase;
 
     void Awake()
     {
@@ -21,11 +28,59 @@ public class MovingSphere : MonoBehaviour
         playerInput.x = Input.GetAxis("Horizontal");
         playerInput.y = Input.GetAxis("Vertical");
         playerInput = Vector2.ClampMagnitude(playerInput, 1f);
-        Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
-        velocity = body.velocity;
+        desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * maxSpeed;
+        desiredJump |= Input.GetButtonDown("Jump");
+    }
+    
+    void FixedUpdate()
+    {
+        UpdateState();
         float maxSpeedChange = maxAcceleration * Time.deltaTime;
         velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
-        velocity.z =  Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+        velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
+        if (desiredJump)
+        {
+            desiredJump = false;
+            Jump();
+        }
         body.velocity = velocity;
+        onGround = false;
+    }
+
+    void Jump()
+    {
+        if (onGround || jumpPhase < maxAirJumps)
+        {
+            jumpPhase += 1;
+            velocity.y += Mathf.Sqrt(-2f * Physics.gravity.y * jumpHeight);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+
+    void EvaluateCollision(Collision collision)
+    {
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            Vector3 normal = collision.GetContact(i).normal;
+            onGround |= normal.y >= 0.9f;
+        }
+    }
+
+    void UpdateState()
+    {
+        velocity = body.velocity;
+        if (onGround)
+        {
+            jumpPhase = 0;
+        }
     }
 }
